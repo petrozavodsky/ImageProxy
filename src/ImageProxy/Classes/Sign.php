2be84cfa9bbdbecf77371c41695ee612ce6dcc07
petrozavodsky <petrozavodsky@gmail.com>
2019-06-06 10:55:37 +0300

    builder method

--- src/ImageProxy/Classes/Sign.php
+++ src/ImageProxy/Classes/Sign.php
@@ -11,13 +11,41 @@ class Sign
 
     private $salt;
 
+    private $signatureSize = 8;
+
     public function __construct()
     {
 
     }
 
 
-    public function sign($data)
+    public function sign($path)
+    {
+        $signatureSize = $this->signatureSize;
+
+        $keyBin = pack("H*", $this->key);
+        if (empty($keyBin)) {
+
+            return new WP_Error('error', 'Key expected to be hex-encoded string');
+        }
+
+        $saltBin = pack("H*", $this->salt);
+
+        if (empty($saltBin)) {
+
+            return new WP_Error('error', 'Salt expected to be hex-encoded string');
+        }
+
+        $signature = hash_hmac('sha256', $saltBin . $path, $keyBin, true);
+
+        $signature = pack('A' . $signatureSize, $signature);
+        $signature = rtrim(strtr(base64_encode($signature), '+/', '-_'), '=');
+
+        return sprintf("/%s%s", $signature, $path);
+
+    }
+
+    public function builder($data, $url)
     {
         $default = [
             'resize' => 'fill',
@@ -26,34 +54,15 @@ class Sign
             'gravity' => 'no',
             'enlarge' => 1,
             'extension' => 'png',
+            'url' => $url
         ];
 
         $data = wp_parse_args($data, $default);
 
+        $url = rtrim(strtr(base64_encode($url), '+/', '-_'), '=');
 
-        $signatureSize = 8;
-        $keyBin = pack("H*", $this->key);
-        if (empty($keyBin)) {
-
-            return new WP_Error('error', 'Key expected to be hex-encoded string');
-        }
-
-        $saltBin = pack("H*", $this->salt);
-        if (empty($saltBin)) {
-
-            return new WP_Error('error', 'Salt expected to be hex-encoded string');
-        }
-
-        $url = 'http://img.example.com/pretty/image.jpg';
-        $encodedUrl = rtrim(strtr(base64_encode($url), '+/', '-_'), '=');
-
-        $path = "/{$data['resize']}/{$data['width']}/{$data['height']}/{$data['gravity']}/{$data['enlarge']}/{$encodedUrl}.{$data['extension']}";
-
-        $signature = hash_hmac('sha256', $saltBin . $path, $keyBin, true);
-        $signature = pack('A' . $signatureSize, $signature);
-        $signature = rtrim(strtr(base64_encode($signature), '+/', '-_'), '=');
-
-        return sprintf("/%s%s", $signature, $path);
+        return "/{$data['resize']}/{$data['width']}/{$data['height']}/{$data['gravity']}/{$data['enlarge']}/{$url}.{$data['extension']}";
 
     }
+
 }
