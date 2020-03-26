14a28fc8cbb67b59b85420307e5d39048ebb7ab0
petrozavodsky <petrozavodsky@gmail.com>
2020-03-26 17:34:34 +0300

    aselect host (fix)

--- src/ImageProxy/Classes/SelectCdnAddress.php
+++ src/ImageProxy/Classes/SelectCdnAddress.php
@@ -6,78 +6,85 @@ namespace ImageProxy\Classes;
 use ImageProxy\Admin\Page;
 use WP_Error;
 
-class SelectCdnAddress {
-	private $salt;
+class SelectCdnAddress
+{
+    private $salt;
 
-	private $addressList = [];
+    private $addressList = [];
 
-	public function __construct( $salt, $addressList ) {
-		$this->salt        = $salt;
-		$this->addressList = $addressList;
+    public function __construct($salt, $addressList)
+    {
+        $this->salt = $salt;
+        $this->addressList = $addressList;
 
-		if ( empty( $salt ) ) {
-			new WP_Error( 'empty_salt', 'pls add salt' );
-		}
+        if (empty($salt)) {
+            new WP_Error('empty_salt', 'pls add salt');
+        }
 
-		if ( empty( $this->addressList ) ) {
-			new WP_Error( 'empty_address', 'pls set addresses array' );
+        if (empty($this->addressList)) {
+            new WP_Error('empty_address', 'pls set addresses array');
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * Получаем хосты из базы
-	 * @return array
-	 */
-	public static function getOptions() {
-		$options = get_option( Page::$slug, [
-			'key'  => '',
-			'salt' => '',
-			'host' => '',
-		] );
+    /**
+     * Получаем хосты из базы
+     * @return array
+     */
+    public static function getOptions()
+    {
+        $options = get_option(Page::$slug, [
+            'key' => '',
+            'salt' => '',
+            'host' => '',
+        ]);
 
-		if ( isset( $options['host'] ) && ! empty( $options['host'] ) ) {
-			$hosts = explode( ',', $options['host'] );
-			$hosts = array_map( 'trim', $hosts );
+        if (isset($options['host']) && !empty($options['host'])) {
+            $hosts = explode(',', $options['host']);
+            $hosts = array_map('trim', $hosts);
 
-			return $hosts;
-		}
+            return $hosts;
+        }
 
-		return [];
-	}
+        return [];
+    }
 
-	/**
-	 * Получение адреса из списка
-	 *
-	 * @return mixed
-	 */
-	public function getAddress() {
+    /**
+     * Получение адреса из списка
+     *
+     * @return mixed
+     */
+    public function getAddress()
+    {
 
-		$number = (int) substr( hexdec( $this->salt ), 0, 1 );
+        $saltHex = preg_replace("#[^a-f\d]#i", '', $this->salt);
 
-		$count = count( $this->addressList );
+        $number = (int)substr(hexdec($saltHex), 0, 1);
 
-		$index = $this->residue( $number, $count );
+        $count = count($this->addressList);
 
-		$index = $index - 1;
+        $index = $this->residue($number, $count);
 
-		return ( isset( $this->addressList[ $index ] ) ? $this->addressList[ $index ] : $this->addressList[0] );
-	}
+        $index = $index - 1;
 
-	/**
-	 * Определение офсета учитывая рамки допустимо мозмодных вариантов
-	 *
-	 * @param $number
-	 * @param $limit
-	 *
-	 * @return mixed
-	 */
-	private function residue( $number, $limit ) {
-		if ( $number > $limit ) {
-			return $this->residue( $number - $limit, $limit );
-		}
+        return (isset($this->addressList[$index]) ? $this->addressList[$index] : $this->addressList[0]);
+    }
 
-		return $number;
-	}
+    /**
+     * Определение офсета учитывая рамки допустимо  возмодных вариантов
+     *
+     * @param $number
+     * @param $limit
+     *
+     * @return mixed
+     */
+    private function residue($number, $limit)
+    {
+        if ($number > $limit) {
+            return $this->residue($number - $limit, $limit);
+        }
+
+        return $number;
+    }
 }
 
