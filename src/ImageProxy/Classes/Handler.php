f2665cbc2270f7b8252cb7ffdba7b6ecdded32b7
petrozavodsky <petrozavodsky@gmail.com>
2020-05-23 23:04:41 +0300

    Handler method

--- src/ImageProxy/Classes/Handler.php
+++ src/ImageProxy/Classes/Handler.php
@@ -6,4 +6,21 @@ namespace ImageProxy\Classes;
 
 class Handler {
 
+	private $proxy;
+
+	public function __construct() {
+		$this->proxy = new Builder();
+	}
+
+	/**
+	 * @param string $url image url
+	 * @param array $args proxy params
+	 *
+	 * @return string converted image url
+	 */
+	public function convert( $url, $args = [] ) {
+
+		return $this->proxy->builder( $args, $url );
+	}
+
 }
